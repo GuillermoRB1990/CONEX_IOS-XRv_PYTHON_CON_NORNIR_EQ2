173663653ebcac9010a0a011faae622517c9012e
gynPP <rapidoyfuriosoenelsalondeclas@gmail.com>
2021-08-13 16:22:12 -0500

    Archivo IOX-XRv
    Archivo principal, Script

---
+++ IOS-XRv.py
@@ -0,0 +1,13 @@
+from nornir import InitNornir
+from nornir_utils.plugins.functions import print_result
+from nornir_netmiko.tasks import netmiko_send_config, netmiko_send_command
+
+nr = InitNornir(
+    config_file = "C:/Users/JP/Desktop/DevNet_IOSX/config.yaml", dry_run=True
+)
+s1=nr.run(netmiko_send_config, config_file= "IOSXRv.txt")
+s2=nr.run(netmiko_send_config, config_commands=["interface lo 7", "description Equipo2NornirConPython","ipv4 add 7.7.7.7 255.255.255.255", "commit"])
+s3=nr.run(netmiko_send_command, command_string="do show ip int brief")
+print_result(s1)
+print_result(s2)
+print_result(s3)
